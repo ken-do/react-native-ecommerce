f41aa8539eb26c6a241ea9b9cccc9528eaa95d2b
Ken Do <khuong.a.do@gmail.com>
2019-10-20 11:17:14 +0700

    set up to serve static files

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import * as express from 'express';
+import * as path from 'path';
+import * as serveStatic from 'serve-static';
 import ProductController from './server/controllers/ProductController';
 
 const app = express();
+
+app.use(serveStatic(path.join(__dirname, 'server', 'static')));
 
 app.get('/api/products', (req, res) => {
     const productController = new ProductController;
