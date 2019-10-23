dd54b3c6a79144794b50a91e613ff69262bfe2fc
Ken Do <khuong.a.do@gmail.com>
2019-10-23 20:49:33 +0700

    fix import error in CartTotalItem

--- app/components/cart/CartTotal.tsx
+++ app/components/cart/CartTotal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FlatList, View, Text, StyleSheet, ScrollView } from 'react-native';
 import CartTotalItem from './CartTotalItem';
-import { ICartItem } from './CartItem';
+import ICartItem from '../../interfaces/CartItem';
 
 interface IProps {
     items: ICartItem[],
