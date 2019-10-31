727c0eb8b5a81862f4a0de1594164a5d5cd9f102
Ken Do <khuong.a.do@gmail.com>
2019-10-31 21:03:06 +0700

    add Helper class

---
+++ app/ultilities/Helper.ts
@@ -0,0 +1,9 @@
+export default class {
+    static getFirstKey(o: object) {
+        return Object.keys(o)[0];
+    }
+
+    static getFirstValue(o: object) {
+        return Object.values(o)[0];
+    }
+}
